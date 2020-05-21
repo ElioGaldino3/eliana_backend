010dd64ea546275e72a684dd818570637254065e
Elio Galdino <eliogaldino79@outlook.com>
2020-05-21 19:46:37 -0300

    210520

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 require("dotenv").config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Eliana API')
+    .setDescription('Backend para aplicação que gerencia encomendas')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 4565);
 }
 bootstrap();
